dcd2fcb1dbf4b8f71b872355dec3e4cddc46bb12
GitNiki <dazent52@gmail.com>
2016-03-11 23:23:30 +0600

    Hello World

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,10 @@
  * Created by 4 on 11.03.2016.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        String s = "Hello Wold!";
+        System.out.print(s);
+    }
+
 }
