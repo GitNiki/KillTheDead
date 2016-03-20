f6b70a9973610bd499697068ee040e01670809fa
kotAndEgor <kit4curs@gmail.com>
2016-03-20 22:04:39 +0500

    button add

--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -19,7 +19,6 @@ public class Main extends Application{
         scene.getStylesheets().add("/styles/mainStyle.css");
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
-
         primaryStage.show();
 
     }
