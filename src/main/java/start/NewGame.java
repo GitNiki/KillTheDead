828c4b1138d45ef8a8372c13b18483606ffed5af
kotAndEgor <kit4curs@gmail.com>
2016-03-22 08:04:08 +0500

    new game add image and comment

--- src/main/java/start/NewGame.java
+++ src/main/java/start/NewGame.java
@@ -3,6 +3,8 @@ package start;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -13,11 +15,24 @@ public class NewGame {
 
     public static Scene sceneNewGame;
 
+    /*Картинки инициализируются тут*/
+    private Image backgroundImage = new Image("/images/background.jpg");
+
+    /*Определяем ImageView на основе Image*/
+    private ImageView backgroundImageView = new ImageView(backgroundImage);
+
     public void showScene() {
+
         Group root = new Group();
+        /*Добавление в группу(на сцену)*/
+        root.getChildren().add(0, backgroundImageView);
+
+        /*Определение сцены в которой находится группа*/
         sceneNewGame = new Scene(root, 640, 640);
+        /*Размещаем сцену в нашем окне, вместо той, которая была раньше*/
         Main.parentStage.setScene(sceneNewGame);
 
+        /*Обработчик на нажатие клавишь клавиатуры*/
         sceneNewGame.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
